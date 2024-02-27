aa8596f9e2f507d03f5cbf9c508fb28095419e3b
Atomico592 <atomico592@gmail.com>
2024-02-27 17:28:54 +0600

    Add necessary files and npm packages

---
+++ constants/icons.js
@@ -0,0 +1,23 @@
+import heart from "../../project_react_native_jobs/assets/icons/heart.png";
+import menu from "../../project_react_native_jobs/assets/icons/menu.png";
+import search from "../../project_react_native_jobs/assets/icons/search.png";
+import filter from "../../project_react_native_jobs/assets/icons/filter.png";
+import left from "../../project_react_native_jobs/assets/icons/left.png";
+import heartOutline from "../../project_react_native_jobs/assets/icons/heart-ol.png";
+import share from "../../project_react_native_jobs/assets/icons/share.png";
+import location from "../../project_react_native_jobs/assets/icons/location.png";
+import chevronLeft from '../../project_react_native_jobs/assets/icons/chevron-left.png'
+import chevronRight from '../../project_react_native_jobs/assets/icons/chevron-right.png'
+
+export default {
+  heart,
+  menu,
+  search,
+  filter,
+  left,
+  heartOutline,
+  share,
+  location,
+  chevronLeft,
+  chevronRight
+};
---
+++ constants/images.js
@@ -0,0 +1,5 @@
+import profile from "../../project_react_native_jobs/assets/images/kemal.jpg";
+
+export default {
+  profile,
+};
