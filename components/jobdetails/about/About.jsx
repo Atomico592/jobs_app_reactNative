e5f8736dc8b9bb47bd6e827fe582901ce10efb41
Atomico592 <atomico592@gmail.com>
2024-02-29 18:53:14 +0600

    Add [id] logic, add about, company, specifics, tabs blocks

--- components/jobdetails/about/About.jsx
+++ components/jobdetails/about/About.jsx
@@ -5,11 +5,7 @@ import styles from "./about.style";
 const About = ({ info }) => {
   return (
     <View style={styles.container}>
-      <Text style={styles.headText}>About the job:</Text>
-
-      <View style={styles.contentBox}>
-        <Text style={styles.contextText}>{info}</Text>
-      </View>
+      <Text>About the job:</Text>
     </View>
   );
 };
